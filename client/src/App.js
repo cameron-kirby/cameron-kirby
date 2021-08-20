9a6bf397895778fa27645150f75606a297b04046
Cameron Kirby <cameronevankirby@gmail.com>
2021-08-20 15:08:27 -0500

    :sparkles: Add navigation
    Added navigation and header, also added react router and normalize.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,32 @@
+import React, { useState } from "react";
+import { Switch, Route } from "react-router-dom";
 import './App.css';
+import Navigation from "./components/Navigation"
+
 
 function App() {
+    const [navOpen, setNavOpen] = useState(false); // Nav state
+
     return (
         <div className="App">
-            Hello world!
+            <Navigation navOpen={navOpen} setNavOpen={setNavOpen}/>
+                <Switch>
+                    <Route path="/work">
+                        Hello work!
+                    </Route>
+                    <Route path="/profile">
+                        Hello profile!
+                    </Route>
+                    <Route path="/journal">
+                        Hello journal!
+                    </Route>
+                    <Route path="/contact">
+                        Hello contact!
+                    </Route>
+                    <Route path="/">
+                        Hello world!
+                    </Route>
+                </Switch>
         </div>
     );
 }
