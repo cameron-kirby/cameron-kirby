d25a9fc4089982533f7af103f88cb40933f4c743
Cameron Kirby <cameronevankirby@gmail.com>
2021-08-22 13:40:15 -0500

    :sparkles: Init profile page

---
+++ client/src/components/pages/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { StyledProfile } from './Profile.styled'
+
+const Profile = ({ useWindowDimensions }) => {
+    return (
+        <StyledProfile>
+
+        </StyledProfile>
+    )
+}
+
+export default Profile;
