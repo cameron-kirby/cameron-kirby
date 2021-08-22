b26cdbdbb527d0c0ec0cbff68ce25d4b88a80802
Cameron Kirby <cameronevankirby@gmail.com>
2021-08-22 15:01:00 -0500

    :sparkles: Add profile sections
    Added the profile sections for blurbs and skills with filler content to be changed later

--- client/src/components/pages/Profile/Profile.js
+++ client/src/components/pages/Profile/Profile.js
@@ -11,7 +11,7 @@ const Profile = ({ useWindowDimensions }) => {
                             <button>Link 1</button>
                         </li>
                         <li>
-                            <button>Link 2</button>
+                            <button>My skills</button>
                         </li>
                         <li>
                             <button>Link 3</button>
@@ -21,6 +21,87 @@ const Profile = ({ useWindowDimensions }) => {
                 <div className="landing-text">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget sapien molestie, semper urna ac, pretium diam. Curabitur ultrices sodales felis nec aliquam. Donec ut felis non ligula ultricies tincidunt nec a risus. </p>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget sapien molestie, semper urna ac, pretium diam. Curabitur ultrices sodales felis nec aliquam. Donec ut felis non ligula ultricies tincidunt nec a risus. </p>
+                </div>
+            </section>
+            <section id="profile-blurbs">
+                <div className="section-heading">
+                    <h3>Blurb Section Title</h3>
+                </div>
+                <div className="blurb-row">
+                    <div className="columns">
+                        <div className="blurb-row-title">
+                            <h4>Row title</h4>
+                        </div>
+                        <div className="blurb-row-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget sapien molestie, semper urna ac, pretium diam. Curabitur ultrices sodales felis nec aliquam. Donec ut felis non ligula ultricies tincidunt nec a risus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="blurb-row">
+                    <div className="columns">
+                        <div className="blurb-row-title">
+                            <h4>Row title</h4>
+                        </div>
+                        <div className="blurb-row-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget sapien molestie, semper urna ac, pretium diam. Curabitur ultrices sodales felis nec aliquam. Donec ut felis non ligula ultricies tincidunt nec a risus.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="blurb-row">
+                    <div className="columns">
+                        <div className="blurb-row-title">
+                            <h4>Row title</h4>
+                        </div>
+                        <div className="blurb-row-body">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget sapien molestie, semper urna ac, pretium diam. Curabitur ultrices sodales felis nec aliquam. Donec ut felis non ligula ultricies tincidunt nec a risus.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="profile-skills">
+                <div className="section-heading">
+                    <h3>My skills</h3>
+                </div>
+                <div className="skills-row">
+                    <div className="skills-column">
+                        <div className="column-title">
+                            <h4>Column Title</h4>
+                        </div>
+                        <ul className="column-list">
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                        </ul>
+                    </div>
+                    <div className="skills-column">
+                        <div className="column-title">
+                            <h4>Column Title</h4>
+                        </div>
+                        <ul className="column-list">
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                        </ul>
+                    </div>
+                    <div className="skills-column">
+                        <div className="column-title">
+                            <h4>Column Title</h4>
+                        </div>
+                        <ul className="column-list">
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                            <li>skill</li>
+                        </ul>
+                    </div>
                 </div>
             </section>
         </StyledProfile>
--- client/src/components/pages/Profile/Profile.styled.js
+++ client/src/components/pages/Profile/Profile.styled.js
@@ -41,4 +41,73 @@ export const StyledProfile = styled.div`
             }
         }
     }
+
+    #profile-blurbs {
+        .section-heading {
+            padding: 2rem 55px 2rem;
+            border-bottom: 1px solid rgba(159,174,176,.35);
+        }
+        .blurb-row {
+            padding: 3rem 55px;
+            border-bottom: 1px solid rgba(159,174,176,.35);
+
+            :last-child {
+                padding-bottom: 6rem;
+            }
+
+            .columns {
+                width: 83.33333%;
+                display: flex;
+                justify-content: space-between;
+
+                .blurb-row-title {
+                    width: 25%;
+                }
+                
+                .blurb-row-body {
+                    width: 50%;
+                    padding-left: 15px;
+
+                    p {
+                        font-size: 1.5rem;
+                    }
+                }
+            }
+        }
+    }
+
+    #profile-skills {
+        background-color: var(--black);
+        color: var(--white);
+
+        .section-heading {
+            padding: 2rem 55px 2rem;
+            border-bottom: 1px solid rgba(159,174,176,.35);
+        }
+
+        .skills-row {
+            padding: 3rem 55px;
+            display: flex;
+            justify-content: space-between;
+
+            .skills-column {
+                width: 25%;
+
+                :last-child {
+                    width: 33.3333%
+                }
+
+                .column-title {
+                    padding-bottom: 2rem;
+                }
+
+                .column-list {
+
+                    li {
+                        font-size: 1.5rem;
+                    }
+                }
+            }
+        }
+    }
 `
