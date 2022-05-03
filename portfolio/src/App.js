55ca017ea25acf532e7a8c49dbd381ef4018aa94
Cameron <cameronevankirby@gmail.com>
2022-05-02 20:28:20 -0500

    :tada: begin fullpage setup

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactFullpage from '@fullpage/react-fullpage';
+//components
+import { HomeSlide } from "./components"
+const anchors = ["one", "two", "three"]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactFullpage
+        anchors={anchors}
+        navigation
+        navigationTooltips={anchors}
+        sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
+        onLeave={(origin, destination, direction) => {
+          console.log("onLeave event", { origin, destination, direction });
+        }}
+        render={({ state, fullpageApi }) => {
+          console.log("render prop change", state, fullpageApi); // eslint-disable-line no-console
+
+          return (
+            <div>
+              <div className="section">
+                <HomeSlide />
+              </div>
+              <div className="section">
+                <h3>two</h3>
+              </div>
+              <div className="section">
+                <h3>three</h3>
+              </div>
+            </div>
+          );
+        }}
+      />
     </div>
   );
 }
---
+++ portfolio/src/components/HomeSlide/HomeSlide.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components"
+
+export const StyledHomeSlide = styled.div`
+    background-color: #e44453;
+    height: 100vh;
+`
